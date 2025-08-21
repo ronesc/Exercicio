96bd0271db2dd6a966af9d69da56bbb51f8993e2
ronesc <130864295+ronesc@users.noreply.github.com>
2025-08-21 10:07:34 -0300

    alteração no requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+# -*- coding: utf-8 -*-
 import requests
 
 response = requests.get("https://api.frankfurter.app/latest")
 print(response)
+
+import streamlit as st
+
+st.header('Jogando uma moeda')
+
+st.write('Ainda não é um aplicativo funcional. Em construção.')
+
+pip install streamlit
