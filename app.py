5093af081268f42ff85ced28b3abe4af5b6fb09e
ronesc <130864295+ronesc@users.noreply.github.com>
2025-08-22 10:39:27 -0300

    mensagem do commit

--- app.py
+++ app.py
@@ -9,3 +9,111 @@ import streamlit as st
 st.header('Jogando uma moeda')
 
 st.write('Ainda não é um aplicativo funcional. Em construção.')
+import scipy.stats
+import streamlit as st
+import time
+
+st.header('Jogando uma moeda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n): # função que emula o lançamento de uma moeda
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
+
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+
+    import scipy.stats
+import streamlit as st
+import time
+
+st.header('Jogando uma moeda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
+
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    mean = toss_coin(number_of_trials)
+
+    import pandas as pd
+import scipy.stats
+import streamlit as st
+import time
+
+# estas são variáveis persistentes preservadas à medida que o Streamlin executa novamente esse script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
+
+st.header('Jogando uma moeda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
+
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    mean = toss_coin(number_of_trials)
+    git add .
+git commit -am 'version 1'
+git push
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import requests
+
+response = requests.get("https://api.frankfurter.app/latest")
+print(response)
+
+import streamlit as st
+
+st.header('Jogando uma moeda')
+
+st.write('Ainda não é um aplicativo funcional. Em construção.')
